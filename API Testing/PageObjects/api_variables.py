258904d7aa425c76b03da58aba53edd53e4c2a20
Sandip Verma <sandip.verma@indexnine.com>
2026-02-02 15:05:38 +0530

    updated status code

--- API Testing/PageObjects/api_variables.py
+++ API Testing/PageObjects/api_variables.py
@@ -3,8 +3,5 @@ posts_endpoint="/posts"
 
 todos_endpoint="/todos"
 
-get_status_code=200
-post_status_code=201
-put_status_code=200
-patch_status_code=200
-delete_status_code=200
+success_status_code=200
+created_status_code=201
